2a34938693dcbf51d9814ce2f366c4fc34678435
Christian Tyskyi <c.tyskyi@ticketstravelnetwork.com>
2021-05-29 20:22:35 +0300

    markers from api

--- src/components/map/MapAPI.js
+++ src/components/map/MapAPI.js
@@ -1,7 +1,7 @@
 
 export default {
     async getLocations() {
-        const response = await fetch('locations.json');
+        const response = await fetch('http://127.0.0.1:8000/api/marker/');
         return response.json();
     }
 }
--- src/components/map/MapboxComponent.js
+++ src/components/map/MapboxComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import ReactMapGL, { Layer, Marker, Popup, Feature } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './Mapbox.css';
 
@@ -26,10 +26,26 @@ class MapboxComponent extends Component {
             onViewportChange={(viewport) => this.setState({viewport})}
         >
             {this.props.locations.map((location, key) => (
+                // <Layer
+                // type="symbol"
+                // layout={{ "icon-image": "harbor-15" }}>
+                //     <Feature
+                //     coordinates={{
+                //         latitude: Number(location.latitude),
+                //         longitude: Number(location.longitude)
+                //     }}
+                //     onClick={e => {
+                //                     e.preventDefault();
+                //                     this.setState({selectedLocation: location});
+                //                 }}/>
+                // </Layer>
+
                 <Marker
                     key={key}
-                    latitude={location.coords.latitude}
-                    longitude={location.coords.longitude}
+                    latitude={Number(location.latitude)}
+                    longitude={Number(location.longitude)}
+                    offsetLeft={-20} 
+                    offsetTop={-20}
                 >
                     <button
                         className="marker-btn"
@@ -44,14 +60,16 @@ class MapboxComponent extends Component {
             ))}
             {this.state.selectedLocation ? (
                 <Popup
-                    latitude={this.state.selectedLocation.coords.latitude}
-                    longitude={this.state.selectedLocation.coords.longitude}
+                    latitude={Number(this.state.selectedLocation.latitude)}
+                    longitude={Number(this.state.selectedLocation.longitude)}
                     onClose={() => {
                         this.setState({selectedLocation: null});
                     }}
                 >
                     <div className="location-popup">
-                        <img src={'places/'+this.state.selectedLocation.image} alt={this.state.selectedLocation.title} />
+                        {this.state.selectedLocation.mainImage ? 
+                            <img src={'http://127.0.0.1:8000'+this.state.selectedLocation.mainImage.imagePath} alt={this.state.selectedLocation.title} />
+                            : null }
                         <h2>{this.state.selectedLocation.title}</h2>
                         <p>{this.state.selectedLocation.description}</p>
                     </div>
